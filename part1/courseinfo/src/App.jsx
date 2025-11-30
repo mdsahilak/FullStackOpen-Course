e15e8bdf9314ab82d661852eb735cfa4a7ca6dd6
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2025-11-30 15:15:50 +0530

    finish ex 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -21,34 +21,35 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  const sum = props.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
-    <p>Number of exercises = {props.total}</p>
+    <p>Number of exercises = {sum}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const total = [part1, part2, part3].reduce((sum, part) => sum + part.exercises, 0)
+  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={total} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
