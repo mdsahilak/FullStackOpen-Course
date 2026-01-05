59106189e7c00db1fab601f6a0f0a77b6fc69f59
mdsahilak <mdsahilak.dev@gmail.com>
2026-01-05 20:52:46 +0530

    setup scaffolding

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>
+          Test Phonebook
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
