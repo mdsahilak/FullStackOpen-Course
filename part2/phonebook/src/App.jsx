3e5973adef2c4243cee849df898c91dc6196d65f
mdsahilak <mdsahilak.dev@gmail.com>
2026-01-05 21:01:43 +0530

    finish ex 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addButtonClicked = (event) => {
+    event.preventDefault()
+
+    setPersons(persons.concat({ name: newName}))
+    setNewName('')
+  }
 
   return (
-    <>
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button onClick={addButtonClicked} >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       <div>
-        <p>
-          Test Phonebook
-        </p>
+        {persons.map(person => <p key={person.name}>{person.name} </p>)}
       </div>
-    </>
+    </div>
   )
 }
 
