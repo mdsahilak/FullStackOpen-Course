4026629472f05c4cb084ab8ad88a0e2693396638
mdsahilak <mdsahilak.dev@gmail.com>
2026-01-07 21:08:17 +0530

    ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter'
 import PersonList from './PersonList'
 import NewPersonForm from './NewPersonForm'
@@ -6,13 +7,17 @@ import NewPersonForm from './NewPersonForm'
 const App = () => {
   const [search, setSearch] = useState('')
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const data = response.data
+        setPersons(data)
+      })
+  }, [])
+
   const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
   return (
@@ -27,7 +32,7 @@ const App = () => {
 
       <h3>Add a new phone number</h3>
       <NewPersonForm persons={persons} setPersons={setPersons} />
-      
+
     </div>
   )
 }
